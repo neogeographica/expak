8500c73b4974550405c844c2acb7693594e3c9a6
Joel Baxter <joel.baxter@neogeographica.com>
2013-11-19 17:17:50 -0800

    more specific classifiers about supported Python version
    Probably won't bother to push this to PyPI until the 1.0 release though.

--- setup.py
+++ setup.py
@@ -61,6 +61,8 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
     ],
